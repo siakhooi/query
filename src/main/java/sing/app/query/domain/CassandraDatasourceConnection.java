e76d18a6c32eb330a032631ed81295af593791ec
Siak Hooi <siakhooi@gmail.com>
2026-04-14 13:06:37 +0000

    remove unused imports

--- src/main/java/sing/app/query/domain/CassandraDatasourceConnection.java
+++ src/main/java/sing/app/query/domain/CassandraDatasourceConnection.java
@@ -3,7 +3,6 @@ package sing.app.query.domain;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.StreamSupport;
 
